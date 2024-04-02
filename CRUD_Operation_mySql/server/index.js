ba9a5dcf056a4b954f19e74f631dbb096932e789
Pavan G R <115134463+Pavan-gr07@users.noreply.github.com>
2024-04-02 17:43:13 +0530

    Sql connection done

---
+++ CRUD_Operation_mySql/server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "pavangr",
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello this is backend");
+});
+app.get("/employees", (req, res) => {
+  const q = "SELECT * FROM EMPLOYEE";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend");
+});
